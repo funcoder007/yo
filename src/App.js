11fd02caf0933ed21376c9567df0c6b20301ace1
funcoder007 <65153307+funcoder007@users.noreply.github.com>
2022-07-18 19:48:53 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import ListUsersComponent from "./components/ListUsersComponent";
+import CreateUserComponent from "./components/CreateUserComponent";
+import HeaderComponent from "./components/HeaderComponent";
+import FooterComponent from "./components/FooterComponent";
+import CategoryComponent from "./components/CategoryComponent";
+import ViewUserComponent from "./components/ViewUserComponent";
+import UpdateUserComponent from "./components/UpdateUserComponent";
+import CategoryUpdate from "./components/CategoryUpdate";
+import CategoryAddComponent from "./components/CategoryAddComponent";
+
+function App() {
+  return (
+    <div>
+      <Router>
+       
+          <HeaderComponent></HeaderComponent>
+          <div className="container">
+            <Switch>
+            <Route path ="/first" component = {CategoryComponent}></Route>
+             <Route path ="/users" component = {ListUsersComponent}></Route>
+            
+             <Route path ="/adduser" component = {CreateUserComponent}></Route>
+             <Route path ="/updateuser/:id" component = {UpdateUserComponent}></Route>
+             <Route path ="/viewuser/:id" component = {ViewUserComponent}></Route>
+             <Route path ="/categoryupdate/:id" component = {CategoryUpdate}></Route>
+             <Route path ="/addcategory" component = {CategoryAddComponent}></Route>
+            </Switch>
+          </div>
+          {/* <FooterComponent></FooterComponent> */}
+       
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ListUsersComponent.js
@@ -0,0 +1,127 @@
+import React, { Component } from 'react'
+import UserService from '../services/UserService'
+
+export default class ListEmployeeComponent extends Component {
+  
+   constructor(props)
+   {
+    super(props)
+    this.state={
+
+        users : []
+
+    }
+    this.addUser = this.addUser.bind(this);
+    this.editUser = this.editUser.bind(this);
+    this.deleteUser = this.deleteUser.bind(this);
+    this.category = this.category.bind(this);
+ 
+   }
+
+   viewUser(userId)
+   {
+    this.props.history.push(`/viewuser/${userId}`);
+   }
+
+   deleteUser(userId)
+   {
+    UserService.deleteUser(userId).then(
+      res=>{
+
+        this.setState(
+          {
+            users:this.state.users.filter(user=>user.id!= userId)
+          }
+        );
+      }
+    )
+   }
+
+   editUser(id)
+   {
+       this.props.history.push(`/updateuser/${id}`);
+   }
+  
+  componentDidMount()
+  {
+    UserService.getUsers().then((res)=>
+    {
+        this.setState({users : res.data});
+
+    })
+  }
+
+  addUser()
+  {
+     this.props.history.push('/adduser');
+  }
+  category()
+  {
+     this.props.history.push('/first');
+  }
+  
+  
+    render() {
+    return (
+      <div>
+        <br></br>
+         <h2 className= " text-center">Admin Users List</h2>
+
+         <div className='row'>
+         <div className='button'>
+          <button className='btn btn-primary' onClick ={this.addUser}>Add New User</button>
+         </div>
+         <div className='button' >
+          <button className='btn btn-primary' onClick ={this.category}>Categories</button>
+         </div>
+         </div>
+         <br></br>
+         <div className='row'>
+
+          <table className='table table-striped table-bordered'>
+                   <thead>
+                    <tr>
+                        <th>User Name </th>
+                        <th>User Email </th>
+                        <th>User About </th>
+                        <th>Actions </th>
+                    </tr>
+
+
+                   </thead>
+
+
+                   <tbody>
+                           {
+                            this.state.users.map(
+
+
+                              user=>
+                              <tr key ={user.id}>
+                                  <td>{user.name}</td>
+                                  <td>{user.email}</td>
+                                  <td>{user.about}</td>
+                                  <td>
+                                    <button onClick= {()=> this.editUser(user.id)} className='btn btn-info'>Update</button>
+                                    <button style = {{marginLeft:"10px"}} onClick= {()=> this.deleteUser(user.id)} className='btn btn-danger'>Delete</button>
+                                    <button style = {{marginLeft:"10px"}} onClick= {()=> this.viewUser(user.id)} className='btn btn-warning'>View</button>
+                                  </td>
+                                  
+                              </tr>
+
+
+                            )
+                           }
+                   </tbody>
+          </table>
+
+
+
+
+
+         </div>
+
+      </div>
+    )
+  }
+}
